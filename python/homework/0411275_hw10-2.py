38f4d5f10e96551d62b219fa1743af5b832d4b8e
簡志瑋 <dppss92132@gmail.com>
2017-12-28 21:45:37 +0800

    add hw10 modified lab

---
+++ python/homework/0411275_hw10-2.py
@@ -0,0 +1,7 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+html_doc=urllib.request.urlopen("http://www.shopping.com/camera/products?sb=1&CLT=SAS&KW=camera")
+soup = BeautifulSoup(html_doc)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
--- python/lab/lab1.py
+++ python/lab/lab1.py
@@ -1,4 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 import re
 
-f= urllib.request.urlopen(url)
+f= urllib.request.urlopen("https://docs.python.org/3/library/index.html")
+for line in f:
+	
